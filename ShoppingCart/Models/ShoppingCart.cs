3521dacdf353b3a893cbc24affc599d040247e7c
Majoros-Peter <majorospeterur@gmail.com>
2025-01-24 10:05:40 +0100

    kész

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -13,5 +13,21 @@
 
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
 
+        public void AddProduct(string name, double price)
+            => _products.Add(new(name, price));
+
+        public void RemoveProduct(string name)
+        {
+            int index = _products.FindIndex(G => G.Name == name);
+            if (index == -1)
+                throw new InvalidOperationException("Ez így nem lesz jó öcsisajt.");
+            _products.RemoveAt(index);
+        }
+
+        public double GetTotalPrice()
+            => _products.Sum(p => p.Price);
+
+        public ICollection<Product> GetProducts()
+            => _products;
     }
 }
